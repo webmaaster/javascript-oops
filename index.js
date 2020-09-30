3bd565956289ed908cebb5087aa2cb9ee5e43fbe
Software Engineer <softwareengineer@LambdaSr233sMBP.home>
2020-09-30 16:18:24 -0400

    Object created

---
+++ index.js
@@ -0,0 +1,17 @@
+//console.log("Hello Software Engineers!");
+
+const circle = {
+  radius : 7,
+  location: {
+    x: 7,
+    y: 7
+  },
+
+  draw: function() {
+    console.log('draw');
+  }
+
+
+};
+
+circle.draw();
